02b2b71cb3f4bebaf1cea0baa8bbe4bdd12818a4
Lalit Maity <lalitmaity620@gmail.com>
2025-08-22 00:05:23 +0530

    Delete routes/ownerRoutes.js

--- routes/ownerRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import { protect } from "../middleware/auth.js"
-import { changeRoleToOwner } from "../controllers/bikeController.js"
-const ownerRouter=express.Router();
-ownerRouter.post("/change-role",protect,changeRoleToOwner)
-export default ownerRouter;
